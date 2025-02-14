de81382101a465a3d2d1fb01c59f71fddd56155e
shondoe11 <shawnhadritan@gmail.com>
2025-02-14 13:32:34 +0800

    update

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,15 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+
+const saveUser = (req, user) => req.user = user;
+
+const getUser = (req) => req.user;
 
 function verifyToken(req, res, next) {
   try {
-    const token = req.headers.authorization.split(' ')[1];
+    //? Bearer XXX.YYY.ZZZ
+    const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
+
+    //? A.B = C
+    //* saveUser(req, user)
     req.user = decoded.payload;
-    
+
     next();
   } catch (err) {
-    res.status(401).json({ err: 'Invalid token.' });
+    res.status(401).json({ err: "Invalid token." });
   }
 }
 
